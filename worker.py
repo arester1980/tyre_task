5cd70df750d9cf191397e9f49e91badea6ea3919
Arester <temultik@gmail.com>
2020-06-15 16:18:51 +0300

    create(function): parse yml file

---
+++ worker.py
@@ -0,0 +1,20 @@
+import yaml
+from pprint import pprint
+
+
+def pars(file, tag):
+    with open(file, encoding="utf-8") as f:
+        templates = yaml.safe_load(f)
+        pprint(templates)
+        start = templates.find(tag)
+        end = templates.find('</'+tag)
+        value = templates[start:end]
+        return value.split(">")[1]
+
+vendor = pars('items.yml', 'vendor')
+model = pars('items.yml', 'model')
+price = pars('items.yml', 'price')
+
+print(vendor)
+print(model)
+print(price)
